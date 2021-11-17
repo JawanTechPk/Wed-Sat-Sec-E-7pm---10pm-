049e0fb272b7a084770eb84d992544a509afe875
jawantechpk <jawantechpk@gmail.com>
2021-11-17 12:35:14 +0500

    switch statement
    switch statement

---
+++ class 10-11-2021/app.js
@@ -0,0 +1,43 @@
+// var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// var rightNow = new Date();
+
+// var theday = rightNow.getDay();
+
+// var theMotn = rightNow.getMonth();
+
+// // rightNow = rightNow.getDay();
+
+// dayNames = dayNames[3]
+// console.log(dayNames)
+
+
+
+// // var dayResult = dayNames[theday];
+// // console.log(theday,dayResult);
+
+
+// var rightNow = new Date();
+
+// var doomsDay = new Date("June 30, 2035");
+
+
+// var rightNowTime = rightNow.getTime()
+
+// var doomsDayTime = doomsDay.getTime();
+
+
+// // console.log(rightNowTime,doomsDayTime);
+
+// var miliSeCResult = doomsDayTime - rightNowTime;
+
+
+// var result =  miliSeCResult / (1000 * 60 * 60 * 24);
+
+// console.log(result,"Days");
+
+
+
+var d = new Date();
+ d.setFullYear(2001);
+console.log(d)
---
+++ class 28 13-11-2021/app.js
@@ -0,0 +1,127 @@
+// function myFunc(){ 
+//     alert('Hello World'); 
+// }
+
+
+
+// function myFunc(){ 
+//     alert('Hello World'); 
+// }
+
+// myFunc()
+
+
+// function myFunc(userName){ //// Data Get in  function called Parameter
+//     alert('Hello '+userName); 
+// }
+
+
+// myFunc("Umair");  //// Data Pass in calling function called Argument
+
+
+
+// // myFunc("Musadiq");
+
+
+
+
+
+
+
+
+
+// var userNames ;
+// console.log(userNames,'userNames')
+
+// function myFunc(userName,num,address){ //// Data Get in  function called Parameter
+//     console.log('Hello ',userName,num,address); 
+// }
+
+
+// myFunc("Umair",3,["Karachi","lahore"]);  //// Data Pass in calling function called Argument
+
+
+
+// myFunc("Musadiq");
+
+
+
+
+
+
+
+
+
+
+
+// var arr = ["umair",]
+
+// console.log(numerOne);
+
+// function myFunc(userName,num,address){ 
+//     console.log('Hello ',userName,num,address); 
+// }
+
+
+// myFunc("Umair",3,["Karachi","lahore"]); 
+
+
+
+// myFunc("Musadiq");
+
+
+// var userName =  prompt("Enter Your Name")
+
+
+// function add(num1,num2,num3){ 
+//     var result = num1+num2+num3;
+// console.log(result)
+//     return result
+//     }
+    
+    
+//     // console.log(add(2,3,5))
+
+//     var res = add(2,3,5);
+
+//     // alert(res)
+//     console.log(res)
+
+
+
+
+
+
+
+
+
+// console.log(prompt("Enter Your Name"))
+
+
+
+// function add(num1,num2,num3){ 
+//     var result = num1+num2+num3;
+// console.log(result)
+//     return result
+//     }
+    
+    
+//     var res = add(2,3,5);
+
+
+//     console.log(res)
+
+
+var num2 = 4    
+
+
+function addNum(num1){
+var result = num1 + num2
+console.log(result)
+
+}
+
+
+console.log(num1)
+
+addNum(2);
