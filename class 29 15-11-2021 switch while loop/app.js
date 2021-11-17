4e6fc96c899bf9f589a9f784807ca8ec596f759e
jawantechpk <jawantechpk@gmail.com>
2021-11-17 12:36:22 +0500

    while loop
    while loop

---
+++ class 29 15-11-2021 switch while loop/app.js
@@ -0,0 +1,85 @@
+// // if (dayOfWk === "Sat" || dayOfWk === "Sun") {
+// //   alert("Whoopee!");
+// // } else if (dayOfWk === "Fri") {
+// //   alert("TGIF!");
+// // } else {
+// //   alert("Shoot me now!");
+// // }
+
+// // var dayOfWk = "Sun"
+
+// // switch(dayOfWk){
+// //     case "Sat":
+// //         alert("Whoopee!");
+// //         break;
+// //     case "Sun":
+// //         alert("Whoopee!");
+// //         break;
+// //     case "Fri":
+// //         alert('TGIF');
+// //         break;
+// //     default:
+// //         alert("Shoot me now!");
+// // }
+
+
+
+// // var dayOfWk = ""
+
+// // switch(dayOfWk){
+// //     case 6:
+// //         alert("Whoopee!");
+// //         break;
+// //     case 0:
+// //         alert("Whoopee!");
+// //         break;
+// //     case 5:
+// //         alert('TGIF');
+// //         break;
+// //     default:
+// //         alert("Shoot me now!");
+// // }
+
+
+// // var dayOfWk = 6
+
+// // switch(dayOfWk){
+// //     case 6:
+// //         alert("Whoopee!");
+      
+// //     case 0:
+// //         alert("Whoopee!");
+       
+// //     case 5:
+// //         alert('TGIF');
+      
+// //     default:
+// //         alert("Shoot me now!");
+// // }
+
+
+
+
+// // for (var index = 0; index < array.length; index++) {
+   
+    
+// // }
+
+
+// var index = 0;
+// for ( index < array.length ) {
+   
+//     index++
+// }
+
+// var index = 0;
+// while ( index < array.length ) {
+   
+//     index++
+// }
+
+
+// var index = 0;
+// do{
+//     index++
+// }while ( index < array.length ) 
